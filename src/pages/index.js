3d8d85ff556e4f961b50d0ea72ba9d015514a4ba
Craig Gallimore <73689369+GalliWare@users.noreply.github.com>
2021-02-16 14:50:59 +0200

    added more custom scaling styles so that the ipad had a fixed mobile version and not a mixed breaken Full web version.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,11 @@ function Index() {
   return (
     <ChakraProvider>
       <Box backgroundColor="#222222" width="100%" position="fixed" height="100%"></Box>
-      <Image src="/indexImage.jpg" width={["100%", "100%", "50%"]} position="fixed"></Image>
-      <Box display={["inherit", "inherit", "none"]} width="100%" position="fixed" height="100%" bgGradient="linear(to-t, #222222, #222222, #222222, transparent , transparent)"></Box>
-      <Box display={["none", "none", "inherit"]} width="50%" position="fixed" height="150%" bgGradient="linear(to-l, #222222,#222222,#222222, transparent)" transform="rotate(12deg)" marginLeft="20%" marginTop="-10%"></Box>
-      <Box position="fixed" width={["100%", "50%"]} marginLeft={["0", "45%"]} height="100%">
-        <Flex direction="column" marginTop={["100%", "50%", "33%"]}>
+      <Image src="/indexImage.jpg" width={["100%", "100%", "100%", "100%", "50%"]} position="fixed"></Image>
+      <Box display={["inherit", "inherit", "inherit", "inherit", "none"]} width="100%" position="fixed" height="100%" bgGradient="linear(to-t, #222222, #222222, #222222, transparent , transparent)"></Box>
+      <Box display={["none", "none", "none", "none", "inherit"]} width="50%" position="fixed" height="150%" bgGradient="linear(to-l, #222222, #222222, #222222, #222222, #222222, transparent, transparent, transparent)" transform="rotate(12deg)" marginLeft="20%" marginTop="-10%"></Box>
+      <Box position="fixed" width={["100%", "100%", "100%", "100%", "50%"]} marginLeft={["0", "0", "0", "0", "45%"]} height="100%">
+        <Flex direction="column" marginTop={["100%", "75%", "75%", "75%", "33%"]}>
           <Heading padding="4" fontFamily="verdana" color="yellow" fontSize={["3xl", "5xl"]}>
             Software Developer
           </Heading >
