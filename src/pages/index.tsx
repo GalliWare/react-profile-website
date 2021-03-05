cc75baa83ec81f7b6fde25a3d1274366f214644a
Craig Gallimore <c.gallimore.it@gmail.com>
2021-03-05 13:17:06 +0200

    Refacture and adding of array for the text to break it down better. Added todo for moving content to api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,25 +3,54 @@ import { ChakraProvider, Image, Box, Text, Heading, Flex } from "@chakra-ui/reac
 import Header from "../components/Header"
 
 function Index() {
-  let heading = process.env.NEXT_PUBLIC_INDEX_HEADING
-  let text = process.env.NEXT_PUBLIC_INDEX_TEXT
+  // TODO: move env data into api.
+  const heading = process.env.NEXT_PUBLIC_INDEX_HEADING
+  const textAlignmentValues = ["center", "center", "center", "center", "center", "inherit"]
+  const text = [
+    <Text key="p1" as="kbd" color="white" alignContent={textAlignmentValues}>{process.env.NEXT_PUBLIC_INDEX_TEXT_P1}
+    </Text>,
+    <Text key="p2" as="kbd" color="white" alignContent={textAlignmentValues}>{process.env.NEXT_PUBLIC_INDEX_TEXT_P2}
+    </Text>,
+    <Text key="p3" as="kbd" color="white" alignContent={textAlignmentValues}>{process.env.NEXT_PUBLIC_INDEX_TEXT_P3}
+    </Text>,
+    <Text key="p4" as="kbd" color="white" alignContent={textAlignmentValues}>{process.env.NEXT_PUBLIC_INDEX_TEXT_P4}
+    </Text>,
+  ]
+  const displayValues = ["inherit", "inherit", "inherit", "inherit", "inherit", "none"]
+  const imgWidthValues = ["100%", "100%", "100%", "100%", "100%", "100%"]
+  const textTopValues = ["100%", "75%", "75%", "75%", "50%", "33%"]
+
   return (
     <ChakraProvider>
-      <Box backgroundColor="#222222" width="100%" position="fixed" height="100%"></Box>
-      <Image src="/indexImage.jpg" width={["100%", "100%", "100%", "100%", "50%"]} position="fixed"></Image>
-      <Box display={["inherit", "inherit", "inherit", "inherit", "none"]} width="100%" position="fixed" height="100%" bgGradient="linear(to-t, #222222, #222222, #222222, transparent , transparent)"></Box>
-      <Box display={["none", "none", "none", "none", "inherit"]} width="50%" position="fixed" height="150%" bgGradient="linear(to-l, #222222, #222222, #222222, #222222, #222222, transparent, transparent, transparent)" transform="rotate(12deg)" marginLeft="20%" marginTop="-10%"></Box>
-      <Box position="fixed" width={["100%", "100%", "100%", "100%", "50%"]} marginLeft={["0", "0", "0", "0", "45%"]} height="100%">
-        <Flex direction="column" marginTop={["100%", "75%", "75%", "75%", "33%"]}>
-          <Heading padding="4" fontFamily="verdana" color="yellow" fontSize={["3xl", "5xl"]} align="center">
-            {heading}
-          </Heading >
-          <Text as="kbd" color="white" padding="4" align="center">
+      <Box
+        backgroundColor="#222222" width="100%" position="fixed" height="100%">
+        <Image
+          src="/indexImage.jpg"
+          width={imgWidthValues}
+          position="fixed">
+        </Image>
+        <Box
+          display={displayValues}
+          width="100%" position="fixed" height="100%"
+          bgGradient="linear(to-t, #222222, #222222, #222222, transparent , transparent)">
+        </Box>
+        <Box
+          position="fixed"
+          width={imgWidthValues}
+          height="100%">
+          <Flex
+            direction="column"
+            marginTop={textTopValues}>
+            <Heading
+              padding="4" fontFamily="verdana" color="yellow"
+              fontSize={["3xl", "5xl"]} alignContent={textAlignmentValues}>
+              {heading}
+            </Heading >
             {text}
-          </Text>
-        </Flex>
+          </Flex>
+        </Box>
+        <Header />
       </Box>
-      <Header />
     </ChakraProvider>
   )
 }
