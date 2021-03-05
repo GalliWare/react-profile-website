abd386839d9195eb6ddc1d7a50b172f0c4be2c64
Craig Gallimore <c.gallimore.it@gmail.com>
2021-03-05 13:15:57 +0200

    improved the button setup by moving this to a array that is added with a for loop. Now to add buttons we can just add more pages and update the paths and buttonNames variables

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,46 +3,51 @@ import { HamburgerIcon } from "@chakra-ui/icons"
 import Link from "next/link"
 
 function Header() {
+  const paths = ["/", "/about", "/projects", "/contact"]
+  const buttonNames = ["Home", "About", "Projects", "Contact"]
+  let buttons = []
+
+  for (let i = 0; i < paths.length; i++) {
+    buttons.push(
+      <Button
+        key={buttonNames[i]}
+        size="lg"
+        color="lightgreen"
+        fontWeight="bold"
+        variant="ghost"
+        _hover={{ borderBottom: "8px" }}>
+        <Link href={paths[i]}>
+          {buttonNames[i]}
+        </Link>
+      </Button>
+    )
+  }
+
   return (
     <ChakraProvider>
       <Flex
         p={["0", "1", "3"]}
         justify="space-between"
         as="nav"
-        wrap="wrap"
-      >
-        <Avatar name="Craig Gallimore" src="/Avatar.png" display={["none", "none", "inherit"]}></Avatar>
+        wrap="wrap">
+        <Avatar
+          name="Craig Gallimore"
+          src="/Avatar.png"
+          display={["none", "none", "inherit"]}>
+        </Avatar>
         <Spacer />
-        <Box display={["none", "none", "inherit"]}>
-          <Button size="lg" color="lightgreen" fontWeight="bold" variant="ghost" _hover={{ borderBottom: "8px" }}>
-            <Link href="/">
-              Home
-            </Link>
-          </Button>
-          <Button size="lg" color="lightgreen" fontWeight="bold" variant="ghost" _hover={{ borderBottom: "8px" }}>
-            <Link href="/projects">
-              Projects
-            </Link>
-          </Button>
-          <Button size="lg" color="lightgreen" fontWeight="bold" variant="ghost" _hover={{ borderBottom: "8px" }}>
-            <Link href="/contact">
-              Contact
-            </Link>
-          </Button>
-          <Button size="lg" color="lightgreen" fontWeight="bold" variant="ghost" _hover={{ borderBottom: "8px" }}>
-            <Link href="/about">
-              About
-            </Link>
-          </Button>
+        <Box
+          display={["none", "none", "inherit"]}>
+          {buttons}
         </Box>
       </Flex>
-      <Menu display={["inherit", "inherit", "none"]} padding="5">
+      <Menu >
         <MenuButton
           as={IconButton}
           aria-label="Options"
           size="md"
           variant="outline"
-          marginLeft="99%"
+          marginLeft="90%"
           icon={<HamburgerIcon />}
           backgroundColor="lightgreen"
           display={["inherit", "inherit", "none"]}
@@ -54,6 +59,11 @@ function Header() {
             </Link>
           </MenuItem>
           <MenuItem >
+            <Link href="/about">
+              About
+            </Link>
+          </MenuItem>
+          <MenuItem >
             <Link href="/projects">
               Projects
             </Link>
@@ -61,11 +71,6 @@ function Header() {
           <MenuItem >
             <Link href="/contact">
               Contact
-            </Link>
-          </MenuItem>
-          <MenuItem >
-            <Link href="/about">
-              About
             </Link>
           </MenuItem>
         </MenuList>
