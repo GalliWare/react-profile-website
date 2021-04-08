a0dafceaf4820081c0e7525bcb2012b9d223ca9c
Craig Gallimore <c.gallimore.it@gmail.com>
2021-04-08 10:14:18 +0200

    added in the Clock project and title prop for Header component

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,17 +3,19 @@ import { ChakraProvider, Box, Flex } from "@chakra-ui/react"
 import Header from "../components/Header"
 import Calculator from "../components/Calculator"
 import Counter from "../components/Counter"
+import Clock from '../components/Clock';
 
 
 function Projects() {
   const projects = [
     <Calculator />,
-    <Counter />
+    <Counter />,
+    <Clock />
   ]
   return (
     <ChakraProvider>
       <Box backgroundColor="#222222" width="100%" position="fixed" height="100%">
-        <Header />
+        <Header title="Projects" />
         <Flex width="100%" textColor="lightblue" justifyContent="center">
           {projects}
         </Flex>
