c35abed06968af2439e10b9eb2ef066590076d4e
Craig Gallimore <c.gallimore.it@gmail.com>
2021-04-15 10:28:05 +0200

    Removed fontfamily and the no longer needed spacer component

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,5 +1,5 @@
 import { Box } from '@chakra-ui/layout'
-import { Flex, Spacer, Text } from '@chakra-ui/react'
+import { Flex, Text } from '@chakra-ui/react'
 import React from 'react'
 
 function ContactCard({ method, info }) {
@@ -7,8 +7,8 @@ function ContactCard({ method, info }) {
     <Box padding="2" marginLeft="5%">
       {/* TODO: make contacts dynamic, need to make URLs clickable and email mailto me setup */}
       <Flex direction="row">
-        <Text fontFamily="verdana" fontSize="2xl">{method}:</Text>
-        <Text fontFamily="verdana" fontSize="2xl" color="white" marginLeft="2%">  {info}</Text>
+        <Text fontSize="2xl">{method}:</Text>
+        <Text fontSize="2xl" color="white" marginLeft="2%">  {info}</Text>
       </Flex>
     </Box>
   )
