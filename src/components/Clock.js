3bf87b052cbc37671dbc192877d24038cbb66590
Craig Gallimore <c.gallimore.it@gmail.com>
2021-04-15 11:57:19 +0200

    Changed the state updates for the time, added two different types of visual clocks

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,20 +1,79 @@
-import { Box, Heading } from "@chakra-ui/react"
+import { Box, Heading, Circle, Square, CircularProgress } from "@chakra-ui/react"
 import React, { useState, useEffect } from "react"
 
 function Clock() {
-  const [time, setTime] = useState('')
+  const [hour, setHour] = useState()
+  const [minute, setMinute] = useState()
+  const [second, setSecond] = useState()
   useEffect(() => {
-    let timeUpdate = setInterval(() => {
-      setTime(new Date().toLocaleTimeString())
+    let clockUpdate = setInterval(() => {
+      let hour = new Date().getHours()
+      let minute = new Date().getMinutes()
+      let seconds = new Date().getSeconds()
+      setHour(hour)
+      setMinute(minute)
+      setSecond(seconds)
     }, 1000)
 
-    return () => clearInterval(timeUpdate)
+    return () => clearInterval(clockUpdate)
   })
 
   return (
     <Box height="50px" width="250px" padding="5px" align="center" margin="10px">
       <Heading size="lg" marginBottom={4}>Basic Clock </Heading>
-      <Heading color="lightpink" marginBottom={4}>{time}</Heading>
+      <Heading color="lightpink" marginBottom={4}>{(hour + ":" + minute + ":" + second)}</Heading>
+      <Circle size="200px">
+        <Square
+          width="180px"
+          height="6px"
+          bgGradient="linear(to-l, lightpink,lightpink ,transparent, transparent , transparent)"
+          position="fixed"
+          transform={"rotate(" + ((hour * 30) - 90) + "deg)"}
+        />
+        <Square
+          width="120px"
+          height="6px"
+          bgGradient="linear(to-l, lightpink, lightpink, transparent, transparent , transparent)"
+          position="fixed"
+          transform={"rotate(" + ((minute * 6) - 90) + "deg)"}
+        />
+        <Square
+          width="180px"
+          height="3px"
+          bgGradient="linear(to-l, lightpink, lightpink, transparent, transparent , transparent)"
+          position="fixed"
+          transform={"rotate(" + ((second * 6) - 90) + "deg)"}
+        />
+      </Circle>
+      <Box>
+        <CircularProgress
+          size="240px"
+          thickness="5px"
+          trackColor="#222222"
+          color="lightpink"
+          value={((second / 60) * 100)}
+          position="fixed"
+        />
+        <CircularProgress
+          marginLeft="10px"
+          marginTop="10px"
+          size="220px"
+          thickness="5px"
+          trackColor="#222222"
+          color="lightpink"
+          value={((minute / 60) * 100)}
+          position="fixed"
+        />
+        <CircularProgress
+          marginLeft="20px"
+          marginTop="20px"
+          size="200px"
+          thickness="5px"
+          trackColor="#222222"
+          color="lightpink"
+          value={(hour / 12) * 100}
+        />
+      </Box>
     </Box >
   )
 }
