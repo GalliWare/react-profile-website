77ce3bd0b90d821960fcba297c8656eb27006795
Craig Gallimore <c.gallimore.it@gmail.com>
2021-04-15 10:38:24 +0200

    Remove Flex, as no longer needed with the about page changes. Move company to make it easy to see the difference between title and company

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,12 +1,15 @@
-import { Box, Heading } from '@chakra-ui/layout'
-import { Text } from '@chakra-ui/react'
+import { Box, Heading, Flex } from '@chakra-ui/layout'
+import { Text, Link } from '@chakra-ui/react'
 
 function JobCard({ company, jobTitle, startTime, endTime, location }) {
   return (
-    <Box padding="2" marginLeft="5%">
+    <Box padding="2" color="white">
       {/* TODO: see if we can make the company a link to the companies home page */}
-      <Heading fontFamily="verdana" fontSize="3xl">{jobTitle} @{company}</Heading>
-      <Text fontFamily="verdana" marginLeft="1%" color="white" fontSize={["m", "lg"]}>from {startTime} to {endTime} in {location}</Text>
+      <Flex direction="row">
+        <Heading fontSize="3xl" color="lightblue">{jobTitle}</Heading>
+        <Text fontSize="xl" marginLeft="2"> @{company}</Text>
+      </Flex>
+      <Text marginLeft="2%">from {startTime} to {endTime} in {location}</Text>
       {/* TODO: maybe we can add a description sometime or just remove this comment 
           <Text>{description}</Text> 
         */}
