7a98dbd549c48ca369bc62c3a33a0fc3581d1696
Craig Gallimore <c.gallimore.it@gmail.com>
2021-04-16 08:50:13 +0200

    Layout updates, removed flex and moved company element to it's on line.

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,15 +1,13 @@
-import { Box, Heading, Flex } from '@chakra-ui/layout'
-import { Text, Link } from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/layout'
+import { Text } from '@chakra-ui/react'
 
 function JobCard({ company, jobTitle, startTime, endTime, location }) {
   return (
     <Box padding="2" color="white">
       {/* TODO: see if we can make the company a link to the companies home page */}
-      <Flex direction="row">
-        <Heading fontSize="3xl" color="lightblue">{jobTitle}</Heading>
-        <Text fontSize="xl" marginLeft="2"> @{company}</Text>
-      </Flex>
-      <Text marginLeft="2%">from {startTime} to {endTime} in {location}</Text>
+      <Heading fontSize="3xl" color="lightblue" align="center">{jobTitle}</Heading>
+      <Text fontSize="xl" marginLeft="2" align="center"> @{company}</Text>
+      <Text marginLeft="2%" align="center">from {startTime} to {endTime} in {location}</Text>
       {/* TODO: maybe we can add a description sometime or just remove this comment 
           <Text>{description}</Text> 
         */}
