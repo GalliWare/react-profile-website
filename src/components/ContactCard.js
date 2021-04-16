8dcce4514d9aae9e84c3a524491bb78b48c3871c
Craig Gallimore <c.gallimore.it@gmail.com>
2021-04-16 08:53:27 +0200

    Layout improvements

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -4,12 +4,10 @@ import React from 'react'
 
 function ContactCard({ method, info }) {
   return (
-    <Box padding="2" marginLeft="5%">
+    <Box padding="2">
       {/* TODO: make contacts dynamic, need to make URLs clickable and email mailto me setup */}
-      <Flex direction="row">
-        <Text fontSize="2xl">{method}:</Text>
-        <Text fontSize="2xl" color="white" marginLeft="2%">  {info}</Text>
-      </Flex>
+      <Text fontSize="2xl" align="center">{method}:</Text>
+      <Text fontSize="2xl" color="white" align="center">  {info}</Text>
     </Box>
   )
 }
