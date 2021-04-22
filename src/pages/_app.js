470b78daa21875bb01416086434adb34d35a1901
Craig Gallimore <c.gallimore.it@gmail.com>
2021-04-22 17:13:50 +0200

    added _app.js to restructure to a single page site

---
+++ src/pages/_app.js
@@ -0,0 +1,46 @@
+import React from "react"
+import { ChakraProvider, Flex, Tabs, Tab, TabPanels, TabPanel, TabList, extendTheme, Heading } from "@chakra-ui/react"
+import About from "./about"
+import Home from "./home"
+import Projects from "./projects"
+
+
+function App() {
+  const config = {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  }
+  const theme = extendTheme({ config })
+  const displayValues = ["none", "none", "none", "inherit", "inherit", "inherit"]
+  return (
+    <ChakraProvider theme={theme}>
+      <Heading position="fixed" color="yellow" display={displayValues}>Craig Gallimore</Heading>
+      <Tabs align="end" variant="unstyled" color="yellow">
+        <TabList>
+          <Tab _selected={{ color: "black", bg: "yellow" }}>Home</Tab>
+          <Tab _selected={{ color: "black", bg: "yellow" }}>About</Tab>
+          <Tab _selected={{ color: "black", bg: "yellow" }}>Projects</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <Home />
+          </TabPanel>
+          <TabPanel>
+            <Flex width="100%" textColor="yellow" justifyContent="center">
+              <About />
+            </Flex>
+          </TabPanel>
+          <TabPanel>
+            <Flex width="100%" textColor="yellow" justifyContent="center">
+              <Projects />
+            </Flex>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+
+    </ChakraProvider>
+  )
+}
+
+export default App
