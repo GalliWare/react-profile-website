a8dcdce24438915925b60a7655a6f98a89a67a8f
Craig Gallimore <c.gallimore.it@gmail.com>
2021-04-22 17:15:18 +0200

    changed chakraprovider to a box, removed header, improved tabs for a better UI experience

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { ChakraProvider, Flex, Tabs, Tab, TabPanels, TabPanel, TabList } from "@chakra-ui/react"
+import { ChakraProvider, Flex, Tabs, Tab, TabPanels, TabPanel, TabList, Box } from "@chakra-ui/react"
 import Header from "../components/Header"
 import Calculator from "../components/Calculator"
 import Counter from "../components/Counter"
@@ -7,36 +7,35 @@ import Clock from '../components/Clock';
 
 
 function Projects() {
+  const colors = { "headings": "lightgreen" }
   return (
-    <ChakraProvider>
-      <Header title="Projects" />
-      <Tabs align="center">
+    <Box>
+      <Tabs align="center" variant="unstyled" color="lightblue">
         <TabList>
-          <Tab>Clocks</Tab>
-          <Tab>Calculator</Tab>
-          <Tab>Counter</Tab>
+          <Tab _selected={{ color: "black", bg: "lightblue" }}>Clocks</Tab>
+          <Tab _selected={{ color: "black", bg: "lightblue" }}>Calculator</Tab>
+          <Tab _selected={{ color: "black", bg: "lightblue" }}>Counter</Tab>
         </TabList>
 
         <TabPanels>
           <TabPanel>
-            <Flex width="100%" textColor="yellow" justifyContent="center">
+            <Flex width="100%" textColor={colors.headings} justifyContent="center">
               <Clock />
             </Flex>
           </TabPanel>
           <TabPanel>
-            <Flex width="100%" textColor="yellow" justifyContent="center">
+            <Flex width="100%" textColor={colors.headings} justifyContent="center">
               <Calculator />
             </Flex>
           </TabPanel>
           <TabPanel>
-            <Flex width="100%" textColor="yellow" justifyContent="center">
+            <Flex width="100%" textColor={colors.headings} justifyContent="center">
               <Counter />
             </Flex>
           </TabPanel>
         </TabPanels>
       </Tabs>
-
-    </ChakraProvider>
+    </Box>
   )
 }
 
